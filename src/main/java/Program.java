306cbb9f5d327585546440a7f4beb2cba908b703
Roman Pupkov <r2325@ya.ru>
2022-08-12 14:43:39 +0100

    My parameterized test

---
+++ src/main/java/Program.java
@@ -0,0 +1,9 @@
+public class Program {
+
+    private int age;
+    private boolean result;
+
+    public boolean checkIsAdult(int age) {
+        return age >= 18;
+    }
+}
---
+++ src/test/java/CheckIsAdultTest.java
@@ -0,0 +1,35 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+        this.age = age;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTextData() {
+        return new Object[][]{
+                {19, true},
+                {17, false},
+                {22, true},
+                {18, true},
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        boolean isAdult = program.checkIsAdult(age);
+        Assert.assertEquals("Ожидаемый результат не сошелся с фактическим", result, isAdult);
+    }
+}
+
+
